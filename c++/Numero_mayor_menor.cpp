9c64a9e98b2fc833e208b24396a7915b2e32a56a
OscarGRG14 <70034096+OscarGRG14@users.noreply.github.com>
2023-02-07 16:25:03 -0300

    Add files via upload

---
+++ c++/Numero_mayor_menor.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a, b, c;
+	
+	cout << "favor ingrese valor para a.: " << endl;
+	cin >> a;
+	
+	cout << "favor ingrese valor para b.: " << endl;
+	cin >> b;
+	
+	cout << "favor ingrese valor para c.: " << endl;
+	cin >> c;
+	
+	
+	
+	if(a > b and c < a){
+		cout << "El numero " << a << " es mayor a " << b << " y es mayor a " << c << endl;
+	}
+	else{
+		if(a < b and a < c){
+			cout << "El numero " << b << " es mayor a " << a << " y es mayor a " << c << endl;
+		}
+		else{
+			cout << "El numero " << c << " es mayor a " << b << " y es mayor a " << a << endl;
+			
+		}
+	}
+	
+	
+	return 0;
+}
+
